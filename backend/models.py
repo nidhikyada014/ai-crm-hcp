d948bf060d23975a95667f7f3c9a377139140229
nidhikyada014 <nidhikyada014@gmail.com>
2026-03-31 00:56:00 -0700

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+class Interaction(Base):
+    __tablename__ = "interactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hcp_name = Column(String)
+    interaction_type = Column(String)
+    notes = Column(Text)
+    summary = Column(Text)
